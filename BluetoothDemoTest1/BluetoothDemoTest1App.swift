e56e368f31dbe485c325d58acfadf1ab68d8af3e
ardwang <278161009@qq.com>
2021-02-03 08:00:46 +0800

    Initial Commit

---
+++ BluetoothDemoTest1/BluetoothDemoTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  BluetoothDemoTest1App.swift
+//  BluetoothDemoTest1
+//
+//  Created by RND on 2021/2/3.
+//
+
+import SwiftUI
+
+@main
+struct BluetoothDemoTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
